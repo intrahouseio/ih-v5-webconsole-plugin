690a673e97d8038dc29db04a79dd8bdb888a2ca3
Artem Ivanov <fast0490f@mail.ru>
2022-12-27 11:59:23 +0300

    version 5.2.0

--- index.js
+++ index.js
@@ -1,60 +1,97 @@
-const EventEmitter = require('events');
-const spawn = require('child_process').spawn;
+const path = require('path');
 const os = require('os');
 
-class Spawn extends EventEmitter {
-  constructor(opt) {
-    super();
+const pty = require('./pty/index');
 
-    this.isWindows = os.platform() === 'win32';
+const logger = require('./lib/logger');
+
+const sessions = {};
+
+function sendProcessInfo() {
+  const mu = process.memoryUsage();
+  const memrss = Math.floor(mu.rss / 1024);
+  const memheap = Math.floor(mu.heapTotal / 1024);
+  const memhuse = Math.floor(mu.heapUsed / 1024);
+
+  const data = { memrss, memheap, memhuse };
+
+  process.send({ type: 'procinfo', data });
+}
+
+function main(options) {
+  let opt;
+  try {
+    opt = JSON.parse(process.argv[2]);
+  } catch (e) {
+    opt = {};
+  }
+
+  const logfile = opt.logfile || path.join(__dirname, 'ih_webconsole.log');
+  const loglevel = opt.loglevel || 0;
+
+  logger.start(logfile, loglevel);
+  logger.log('Plugin webconsole has started  with args: ' + process.argv[2]);
+
+  setInterval(sendProcessInfo, 10000);
+
+  webconsole(opt);
+}
+
+function webconsole(opt) {
+  process.on('message', msg => {
+    if (typeof msg === 'object' && msg.type === 'transferdata' && msg.method === 'unsub') {
+      const uuid = msg.uuid;
+      if (sessions[uuid] !== undefined) {
+        logger.log(`end session: ${uuid}`)
+        sessions[uuid].destroy();
+        delete sessions[uuid];
+      }
+    }
+
+    if (typeof msg === 'object' && msg.type === 'transferdata' && msg.payload !== undefined) {
+      const uuid = msg.uuid;
+      const data = msg.payload;
+
+      if (data.type === 'start') {
+        logger.log(`new session: ${uuid}`)
+
+        const env = Object.assign({}, process.env);
+        env['COLORTERM'] = 'truecolor';
+  
+        sessions[uuid] = pty.spawn({
+          name: 'xterm-color',
+          cols: data.size.cols,
+          rows: data.size.rows,
+          cwd: opt.cwd,
+          env: env
+        });
     
-    if (this.isWindows) {
-      this.pty = spawn('./pty/winpty.exe', [opt.cols, opt.rows]);
-      this.pty.on('error', err => {
-        this.destroy();
-      });
-      this.pty.stdout.on('data', (data) => {
-        if (this.pty !== null) {
-          this.emit('data', data.toString());
-        }
-      });
-    } else {
-      this.pty = require('node-pty-prebuilt-multiarch').spawn('sudo', ['login'], opt);
-      this.pty.on('data', data => {
-        if (this.pty !== null) {
-          this.emit('data', data);
-        }
-      });
-    }
-  }
+        sessions[msg.uuid].on('data', function(data) {
+          process.send({ 
+            id: uuid, 
+            uuid: uuid, 
+            type: 'transferdata', 
+            unit: 'webconsole',
+            payload: { type: 'data', body: data }, 
+          });
+        }); 
+      }
 
-  write(data) {
-    if (this.pty !== null) {
-      if (this.isWindows) {
-        this.pty.stdin.write(data);
-      } else {
-        this.pty.write(data);
+      if (data.type === 'data') {
+        if (sessions[uuid] !== undefined) {
+          sessions[uuid].write(data.body);
+        }
+      }
+
+      if (data.type === 'end') {
+        if (sessions[uuid] !== undefined) {
+          logger.log(`end session: ${uuid}`)
+          sessions[uuid].destroy();
+          delete sessions[uuid];
+        }
       }
     }
-  }
-
-  destroy() {
-    if (this.pty !== null) {
-      if (this.isWindows) {
-        this.pty.kill('SIGINT');
-      } else {
-        this.pty.destroy();
-      }
-      this.isWindows = null;
-      this.pty = null;
-    }
-  }
+  });
 }
 
-function _spawn(opt) {
-  return new Spawn(opt);
-}
-
-module.exports = {
-  spawn: _spawn,
-}
+main();
--- pty/index.js
+++ pty/index.js
@@ -2,8 +2,6 @@ const EventEmitter = require('events');
 const spawn = require('child_process').spawn;
 const os = require('os');
 
-const ls = spawn('ls', ['-lh', '/usr']);
-
 class Spawn extends EventEmitter {
   constructor(opt) {
     super();
@@ -11,7 +9,15 @@ class Spawn extends EventEmitter {
     this.isWindows = os.platform() === 'win32';
     
     if (this.isWindows) {
-      this.pty = spawn();
+      this.pty = spawn('./pty/winpty.exe', [opt.cols, opt.rows]);
+      this.pty.on('error', err => {
+        this.destroy();
+      });
+      this.pty.stdout.on('data', (data) => {
+        if (this.pty !== null) {
+          this.emit('data', data.toString());
+        }
+      });
     } else {
       this.pty = require('node-pty-prebuilt-multiarch').spawn('sudo', ['login'], opt);
       this.pty.on('data', data => {
@@ -25,7 +31,7 @@ class Spawn extends EventEmitter {
   write(data) {
     if (this.pty !== null) {
       if (this.isWindows) {
-
+        this.pty.stdin.write(data);
       } else {
         this.pty.write(data);
       }
@@ -35,7 +41,7 @@ class Spawn extends EventEmitter {
   destroy() {
     if (this.pty !== null) {
       if (this.isWindows) {
-
+        this.pty.kill('SIGINT');
       } else {
         this.pty.destroy();
       }
@@ -45,10 +51,10 @@ class Spawn extends EventEmitter {
   }
 }
 
-function spawn(opt) {
+function _spawn(opt) {
   return new Spawn(opt);
 }
 
 module.exports = {
-  spawn,
+  spawn: _spawn,
 }
